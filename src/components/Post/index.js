39f3a0aedcc0779b10696501a45c759dce8f7a58
tund <tund@gemvietnam.com>
2024-05-02 14:11:03 +0700

    change title page

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,44 +1,45 @@
 import React from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {postLoadingSelector, postSelector} from "../../redux/selector";
-import {deleteDataThunk, getDataThunk, resetData} from "../../redux/reducers/postSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { postLoadingSelector, postSelector } from "../../redux/selector";
+import {
+  deleteDataThunk,
+  getDataThunk,
+  resetData,
+} from "../../redux/reducers/postSlice";
 import PostItem from "./PostItem";
 
 function Post() {
-    const posts = useSelector(postSelector);
-    const dispatch = useDispatch();
-    const isLoading = useSelector(postLoadingSelector);
+  const posts = useSelector(postSelector);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(postLoadingSelector);
 
-    const handleGetData = () => {
-        dispatch(getDataThunk()).unwrap();
-    }
+  const handleGetData = () => {
+    dispatch(getDataThunk()).unwrap();
+  };
 
-    const handleResetData = () => {
-        dispatch(resetData());
-    }
+  const handleResetData = () => {
+    dispatch(resetData());
+  };
 
-    const handleDeleteData = (id) => {
-        dispatch(deleteDataThunk(id));
-    }
+  const handleDeleteData = (id) => {
+    dispatch(deleteDataThunk(id));
+  };
 
-    return (
-        <div>
-            <p>Posts</p>
-            <button onClick={handleGetData}>Get data</button>
-            <button onClick={handleResetData}>Reset data</button>
-            <p>
-                {`Loading: ${isLoading}`}
-            </p>
+  return (
+    <div>
+      <h2>This is Post list</h2>
+      <button onClick={handleGetData}>Get data</button>
+      <button onClick={handleResetData}>Reset data</button>
+      <p>{`Loading: ${isLoading}`}</p>
 
-            <div>
-                {
-                    posts && posts.map(post =>
-                        <PostItem key={post.id} post={post} onDelete={handleDeleteData}/>
-                    )
-                }
-            </div>
-        </div>
-    )
+      <div>
+        {posts &&
+          posts.map((post) => (
+            <PostItem key={post.id} post={post} onDelete={handleDeleteData} />
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default Post;
